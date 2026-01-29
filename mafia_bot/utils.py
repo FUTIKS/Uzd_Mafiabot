045a37d9afc41af9a8a49db704b44ddb75f0a925
Sardorbek <nsardorbek776@gmail.com>
2026-01-29 21:54:13 +0500

    ok

--- mafia_bot/utils.py
+++ mafia_bot/utils.py
@@ -17,15 +17,14 @@ writing_allowed_groups = {}
 
 async def set_bot_commands(bot: Bot):
     group_commands = [
-        BotCommand(command="profile", description="Guruh ma'lumotlarini ko'rish"),
         BotCommand(command="game", description="Yangi oyinni boshlash"),
-        BotCommand(command="extend", description="O'yin vaqtini uzaytirish"),
         BotCommand(command="start", description="Registratsiyani tugatish va o'yinni boshlash"),
         BotCommand(command="stop", description="Registratsiyani to'xtatish"),
+        BotCommand(command="extend", description="O'yin vaqtini uzaytirish"),
+        BotCommand(command="profile", description="Guruh ma'lumotlarini ko'rish"),
         BotCommand(command="next", description="Keyingi o'yin haqida eslatish"),
         BotCommand(command="top", description="Rating jadvalini ko'rish"),
         BotCommand(command="top7", description="Rating jadvalini ko'rish"),
-        BotCommand(command="top30", description="Rating jadvalini ko'rish"),
         BotCommand(command="leave", description="oyindan chiqish"),
         BotCommand(command="share", description="Do'stlarni chaqirish"),
         BotCommand(command="help", description="Yordam"),
@@ -35,6 +34,7 @@ async def set_bot_commands(bot: Bot):
     private_commands = [
         BotCommand(command="profile", description="Profil ma'lumotlarini ko'rish"),
         BotCommand(command="start", description="Botni ishga tushirish"),
+        BotCommand(command="top30", description="Rating jadvalini ko'rish"),
         BotCommand(command="language", description="Tilni o'zgartirish"),
         BotCommand(command="help", description="Yordam"),
     ]
